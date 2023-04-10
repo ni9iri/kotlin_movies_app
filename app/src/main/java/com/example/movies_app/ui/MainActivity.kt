86cd9d78542383770f178d5f769c102167c07aa8
Quyen Nguyen <quyen.ng1902@gmail.com>
2023-04-10 20:25:52 +0300

    project added

---
+++ app/src/main/java/com/example/movies_app/ui/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.movies_app.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.movies_app.ui.theme.MoviesappTheme
+import com.example.movies_app.viewmodel.MainViewModel
+
+
+class MainActivity : ComponentActivity() {
+    val mainViewModel by viewModels<MainViewModel>()
+
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            MoviesappTheme {
+                NavHost(navController = navController, startDestination = "trendingMovieList") {
+                    composable("trendingMovieList") {
+                        Surface(color = MaterialTheme.colors.background) {
+                            MainList(navController = navController, mainViewModel = mainViewModel)
+                        }
+                    }
+                    composable("movieDetails") {
+                        MovieDetails(mainViewModel.clickedItem)
+                    }
+                }
+                // A surface container using the 'background' color from the theme
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/movies_app/ui/MovieDetails.kt
@@ -0,0 +1,47 @@
+package com.example.movies_app.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.movies_app.model.Constants
+import com.example.movies_app.model.MovieItem
+import com.google.accompanist.coil.rememberCoilPainter
+
+@Composable
+fun MovieDetails(movieItem: MovieItem){
+    Column() {
+        MovieDetailsBanner(movieItem = movieItem)
+        MovieDetailsText(movieItem = movieItem)
+    }
+}
+
+@Composable
+fun MovieDetailsBanner(movieItem: MovieItem){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .height(300.dp)) {
+        Image(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),painter = rememberCoilPainter(request = Constants.BASE_IMAGE_URL+movieItem.backdrop_path) , contentDescription = "")
+    }
+}
+@Composable
+fun MovieDetailsText(movieItem: MovieItem){
+    Column(modifier = Modifier.padding(10.dp)) {
+        movieItem.title?.let {
+            Text(text = it, fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Normal,
+                fontSize = 24.sp)
+        }
+        Text(text = movieItem.overview, Modifier.padding(top=10.dp),fontFamily = FontFamily.Serif,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp)
+    }
+
+}
